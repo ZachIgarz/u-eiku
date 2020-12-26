0d2c26c3566371a3c13aef0df1d6a626e656af63
ZachIgarz <huffyh00@hotmail.com>
2020-12-25 21:37:29 -0500

    feat: say hi

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"golang.org/x/net/websocket"
+)
+
+const (
+	hi = "Hola"
 )
 
 func hello(c echo.Context) error {
@@ -13,7 +20,9 @@ func hello(c echo.Context) error {
 		defer ws.Close()
 		for {
 			// Write
-			err := websocket.Message.Send(ws, "Hello, Client!")
+
+			//err := websocket.Message.Send(ws, "Hello, Client!")
+			err := websocket.Message.Send(ws, sendMesajeDayOrNight())
 			if err != nil {
 				c.Logger().Error(err)
 			}
@@ -37,4 +46,29 @@ func main() {
 	e.Static("/", "./public")
 	e.GET("/ws", hello)
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+//Escucha la hora del sistema
+func listenSistemHour() string {
+
+	return fmt.Sprint(time.Now().Hour())
+}
+
+func sendMesajeDayOrNight() string {
+	actualTime := listenSistemHour()
+
+	number, err := strconv.ParseUint(actualTime, 10, 32)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if number > 20 {
+
+		return hi + " Buenas Noches UwU"
+	}
+
+	if number > 17 && number < 20 {
+		return hi + " Buenas tardes"
+	}
+
+	return ""
 }
